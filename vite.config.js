1421876a0b7064f392730c23fb45168c484cd200
BesnikBegunca <bb51184@ubt-uni.net>
2025-03-18 01:29:42 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures correct file paths
-})
+  server: {
+    mimeTypes: {
+      'js': 'application/javascript',
+      'jsx': 'application/javascript'
+    }
+  }
+});
